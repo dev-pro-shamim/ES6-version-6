7f060dea1a5a92dd86a5764e000e1fa9f979eb59
Shamim <shamimjkrworld@gmail.com>
2024-04-06 19:31:25 +0600

    practice

---
+++ es6-array.js
@@ -0,0 +1,120 @@
+    //Function Declaration**
+    
+    
+    // function add(a,b){
+    //     const result = a + b;
+    //     return result;
+    
+    // }
+    
+    
+    // function add(a,b){
+        
+    //     return a + b;
+    
+    // }
+    
+    
+    // const sum = add(1,2);
+    // console.log(sum);
+    
+    //function expressio
+    
+    const add2 = function(a,b){
+        return a+b;
+    }
+    
+    
+    const sums = add2(1,2);
+    console.log(sums);
+    
+    //***** */
+
+    //Arrow function
+    const add3 = (a,b) => a+b;
+    
+    const sum = add3(1,2);
+    console.log(sum);
+
+    //Multiply
+    const multiply = (a,b) => a*b;
+    
+    const product = multiply(10,20);
+    console.log(product);
+    //division
+    const divide = (a,b) => a/b;
+    
+    const result = divide(100,20);
+    console.log(result);
+    //subtraction
+    const subtract = (a,b) => a-b;
+    
+    const difference = subtract(100,20);
+    console.log(difference);
+    //modulus
+    const modulus = (a,b) => a%b;
+    
+    const remainder = modulus(100,20);
+    console.log(remainder);
+    //exponentiation
+    const exponent = (a,b) => a**b;
+    
+    const power = exponent(10,2);
+    console.log(power);
+    //square root
+    const sqrt = (a) => Math.sqrt(a);
+    
+    const squareRoot = sqrt(100);
+    console.log(squareRoot);
+    //cube root
+    const cbrt = (a) => Math.cbrt(a);
+    
+    const cubeRoot = cbrt(100);
+    console.log(cubeRoot);
+    //absolute value
+    const abs = (a) => Math.abs(a);
+    
+    const absoluteValue = abs(-100);
+    console.log(absoluteValue);
+
+    //floor division
+    const floor = (a,b) => Math.floor(a/b);
+    
+    const floorDivision = floor(100,20);
+    console.log(floorDivision);
+    //ceiling division
+    const ceil = (a,b) => Math.ceil(a/b);
+    
+    const ceilingDivision = ceil(81,20);
+    console.log(ceilingDivision);
+
+    //round division
+    const round = (a,b) => Math.round(a/b);
+    
+    const roundDivision = round(121,20);
+    console.log(roundDivision);
+    //trunc division
+    const trunc = (a,b) => Math.trunc(a/b);
+    
+    const truncDivision = trunc(121,20);
+    console.log(truncDivision);
+    //log division
+    const log = (a,b) => Math.log(a/b);
+    
+    const logDivision = log(121,20);
+    console.log(logDivision);
+    //log10 division
+    const log10 = (a,b) => Math.log10(a/b);
+    
+    const log10Division = log10(121,20);
+    console.log(log10Division);
+    //log2 division
+    const log2 = (a,b) => Math.log2(a/b);
+    
+    const log2Division = log2(121,20);
+    console.log(log2Division);
+    //log1p division
+    const log1p = (a,b) => Math.log1p(a/b);
+    
+    const log1pDivision = log1p(121,20);
+    console.log(log1pDivision);
---
+++ templete-dynamicString.js
@@ -0,0 +1,25 @@
+const first = 'Shamim'
+const last = 'Islam'
+const great = 'DevOPs Engineer';
+const name = first + ' ' + last+ ' is a ' + great;
+console.log(name);
+
+
+const a = 10;
+const b = 20;
+const result = 'Your Result is : '+ a + ' '+ b + ' and Sum is : ' + (a+b);
+console.log(result);
+
+///********* */
+const math = `The Sum of ${a} and ${b} is ${a+b}`;
+console.log(math);
+
+//Backtick
+const email = `helloDevs@gmail.com`;
+console.log(email);
+
+//**************** */
+const names = `shamim 
+islam
+devops`;
+console.log(names);
