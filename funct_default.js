d89a9a74bd7be9b47d4a1a4586b9b0f576463bfb
Shamim <shamimjkrworld@gmail.com>
2024-04-06 17:43:22 +0600

    first commit

---
+++ funct_default.js
@@ -0,0 +1,21 @@
+function numbers(num1,num2 = 0){
+    const sum = num1 + num2;
+    console.log(num1,num2,sum);
+    return sum;
+}
+
+numbers(10,20); //30
+
+//** */ 
+
+
+function fullName (firstName,lastName = ''){
+    const full = 'firstName'+ ' '+ 'lastName';
+    return full;
+}
+//** */
+function friends(numbs = []){
+    return numbs;
+}
+
+
---
+++ intro.js
@@ -0,0 +1,65 @@
+// const students = {
+//     name : 'shamim',
+//     age : 20,
+//     address : 'khansamah',
+//     city : 'dhaka',
+//     country : 'bangladesh'
+
+
+
+
+// }
+// students.age = 21;
+// students.name = 'shamim ahmed';
+
+// console.log(students);
+// console.log(students.name);
+// console.log(students.age);
+// console.log(students.address);
+// console.log(students.city);
+// console.log(students.country);
+  
+// //===========
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+//  let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+//===========
+
+// let shamim = 20;
+// let ahmed = 21;
+// let result = shamim + ahmed;
+// console.log(result);
+// result = shamim - ahmed;
+// console.log(result);
+// result = shamim * ahmed;
+// console.log(result);
+// result = shamim / ahmed;
+// console.log(result);
+// result = shamim % ahmed;
+// console.log(result);
+// result = shamim ** ahmed;
+// console.log(result);
+
+//create a calculator with javascript
+
+
+// create a function call to be afia
+
+function afia(){
+    console.log('afia is a good girl');
+}
+afia();
+//create a function call to be shamim
+function shamim(){
+    console.log('shamim is a good boy');
+}
+shamim();
+//create a inheritance call to be shamim and afia
